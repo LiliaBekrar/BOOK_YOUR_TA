9e8d72120594ad8ff4b1c30a97ebeff863c42e19
isabelledouin <isabelle.douin@orange.fr>
2021-12-14 13:17:23 +0100

    teachers controller-model-view

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,59 @@
 class TeachersController < ApplicationController
+  before_action :set_teacher, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:new, :create]
+  def new
+    @teacher = Teacher.new
+  end
+
+  def index
+    @teachers = Teacher.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def booking_teacher_list
+    @bookings_teacher = @teacher.bookings
+  end
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+    @teacher.user = @user
+
+    if @teacher.save
+      render :root, notice: 'Teacher was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @teacher.update(teacher_params)
+      redirect_to :root, notice: 'Teacher was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  # Only allow a list of trusted parameters through.
+  def teacher_params
+    params.require(:teacher).permit(:description, :price, :speciality, :city_name, :user_id)
+  end
 end
