e93a6f89e1cc370e5044a02966a78284b09210af
Lilia Bekrar <liliarossi2504@gmail.com>
2021-12-14 10:17:41 +0100

    correction syntax

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < ApplicationRecord
   belongs_to :user
-  has_many :bookings, dependent: destroy
+  has_many :bookings, dependent: :destroy
   validates :description, presence: true, length: { minimum: 5, maximum: 250 }
   validates :price, presence: true
   has_one_attached :photo
