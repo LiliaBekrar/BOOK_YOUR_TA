790b91a7dde0004b9fb8aaff3bcd4497bdd24f61
Lilia Bekrar <liliarossi2504@gmail.com>
2021-12-16 16:04:39 +0100

    profil page waiting acepted, and seed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :teachers, only: [:index, :edit, :update, :show, :new, :create, :destroy] do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:destroy, :show, :edit, :update, :destroy]
+  resources :bookings, only: [:destroy, :show, :edit, :update] do
+    member do
+      get :confirm
+    end
+  end
   get 'profil', to: 'pages#profil'
 end
