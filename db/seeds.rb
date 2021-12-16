a10694c2670b8a718c816ce9f0d279671c0067ce
Lilia Bekrar <liliarossi2504@gmail.com>
2021-12-16 11:43:27 +0100

    add photo to TA

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ user = User.create!(email: "Jeanloup@ta.com", first_name: "Jean Loup", last_name
 p "#{user.email} created"
 
 teacher = Teacher.new(description: "Je suis un super TA", specialty: "JavaScript", price: "56", city_name: "Toulon", user_id: user.id)
-# teacher.photo.key = "Jean_loup_tgx3wa"
+teacher.photo.attach(io: File.open(Rails.root.join('app/assets/images/Jean_loup.jpg')), filename: 'Jean_loup.jpg')
 teacher.save!
 p "#{teacher.user.first_name} created"
 
@@ -22,7 +22,8 @@ user2 = User.create!(email: "Bruno@ta.com", first_name: "Bruno", last_name: "Gue
 p "#{user2.email} created"
 
 teacher = Teacher.new(description: "Homme très dynamique spécialiste des groupes difficiles", specialty: "Ruby", price: "99", city_name: "Marseille", user_id: user2.id)
-# teacher.photo.key = "Bruno_jcdar1"
+teacher.photo.attach(io: File.open(Rails.root.join('app/assets/images/Bruno.jpg')), filename: 'Bruno.jpg')
+
 teacher.save!
 p "#{teacher.user.first_name} created"
 
@@ -30,7 +31,7 @@ user3 = User.create!(email: "Pilou@ta.com", first_name: "Pierre Loic", last_name
 p "#{user3.email} created"
 
 teacher = Teacher.new(description: "Je viens de loin, citoyen du monde, j'aime les photos non-contractuelles", specialty: "SQL", price: "60", city_name: "Lille", user_id: user3.id)
-# teacher.photo.key = "Pilou_dlkb9q"
+teacher.photo.attach(io: File.open(Rails.root.join('app/assets/images/Pilou.jpg')), filename: 'Pilou.jpg')
 teacher.save!
 p "#{teacher.user.first_name} created"
 
