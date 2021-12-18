bdcb6674a14b8f09171270970bfe45e8ae44d471
Lilia Bekrar <liliarossi2504@gmail.com>
2021-12-18 12:40:08 +0100

    correction navbar & dashboard & map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -16,16 +16,19 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+    map.style.position = 'relative';
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
         const element = document.createElement('div');
         element.className = 'marker';
         element.style.backgroundImage = `url('${marker.image_url}')`;
         element.style.backgroundSize = 'contain';
-        element.style.width = '25px';
-        element.style.height = '25px';
+        element.style.width = '35px';
+        element.style.height = '35px';
         element.style.borderRadius = '50%';
         element.style.objectFit = 'cover';
+        element.style.position = 'absolute';
+        element.style.top = '35px';
 
         new mapboxgl.Marker(element)
           .setLngLat([marker.lng, marker.lat])
