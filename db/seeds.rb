42e9e85d666c7170d67049f19ca4f0a0d9388337
Lilia Bekrar <liliarossi2504@gmail.com>
2021-12-20 16:57:45 +0100

    correction price precision and change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.destroy_all
 user = User.create(email: "Jeanloup@ta.com", first_name: "Jean Loup", last_name: "Peres", password: "123456")
 p "#{user.email} created"
 
-teacher = Teacher.new(description: "Développeur Freelance et formé au wagon, je suis fier de pouvoir transmettre à mon tour ce que l'on m'a appris. A part ça j'aime la mer, les bateaux et les div.", specialty: "JavaScript", price: "56", city_name: "Toulon", user_id: user.id)
+teacher = Teacher.new(description: "Développeur Freelance et formé au wagon, je suis fier de pouvoir transmettre à mon tour ce que l'on m'a appris. A part ça j'aime la mer, les bateaux et les div.", specialty: "JavaScript", price: "256", city_name: "Toulon", user_id: user.id)
 teacher.photo.attach(io: File.open(Rails.root.join('app/assets/images/Jean_loup.jpg')), filename: 'Jean_loup.jpg')
 teacher.save
 p "#{teacher.user.first_name} created"
@@ -21,7 +21,7 @@ p "#{teacher.user.first_name} created"
 user2 = User.create(email: "Bruno@ta.com", first_name: "Bruno", last_name: "Guers", password: "123456")
 p "#{user2.email} created"
 
-teacher2 = Teacher.new(description: "HAdventures on sailing boats - Keep learning new coding tricks always - Teaching, sharing knowledge - Impact entrepreneurship", specialty: "Ruby", price: "99", city_name: "Marseille", user_id: user2.id)
+teacher2 = Teacher.new(description: "HAdventures on sailing boats - Keep learning new coding tricks always - Teaching, sharing knowledge - Impact entrepreneurship", specialty: "Ruby", price: "340", city_name: "Marseille", user_id: user2.id)
 teacher2.photo.attach(io: File.open(Rails.root.join('app/assets/images/Bruno.jpg')), filename: 'Bruno.jpg')
 
 teacher2.save
@@ -30,7 +30,7 @@ p "#{teacher.user.first_name} created"
 user3 = User.create(email: "Pilou@ta.com", first_name: "Pierre Loic", last_name: "Pichon", password: "123456")
 p "#{user3.email} created"
 
-teacher3 = Teacher.new(description: "Data Scientist / Ruby on Rails hobbyist Call me Pilou!", specialty: "SQL", price: "60", city_name: "Lille", user_id: user3.id)
+teacher3 = Teacher.new(description: "Data Scientist / Ruby on Rails hobbyist Call me Pilou!", specialty: "SQL", price: "290", city_name: "Lille", user_id: user3.id)
 teacher3.photo.attach(io: File.open(Rails.root.join('app/assets/images/Pilou.jpg')), filename: 'Pilou.jpg')
 teacher3.save
 p "#{teacher3.user.first_name} created"
@@ -41,12 +41,12 @@ p "#{user4.email} created"
 user5 = User.create(email: "georgia@ta.com", first_name: "Georgia", last_name: "Drai", password: "123456")
 p "#{user5.email} created"
 
-teacher4 = Teacher.create(description: "Née à Marseille, travaille dans le transport et la logistique depuis 15 ans. On m’a mis en javascript mais perso choisissez JanLou car j’y touche que dalle..", specialty: "JavaScript", price: "56", city_name: "Lyon", user_id: user5.id)
+teacher4 = Teacher.create(description: "Née à Marseille, travaille dans le transport et la logistique depuis 15 ans. On m’a mis en javascript mais perso choisissez JanLou car j’y touche que dalle..", specialty: "JavaScript", price: "90", city_name: "Marseille", user_id: user5.id)
 
 user6 = User.create(email: "hugo@ta.com", first_name: "Hugo", last_name: "Mancini", password: "123456")
 p "#{user6.email} created"
 
-teacher5 = Teacher.new(description: "Graduated from a business school, I worked for two years in a startup as business analyst and then as business developper. I then opened a bar with some close friends two years ago. Main interests are skateboarding, music and chess.", specialty: "RAILS", price: "56", city_name: "Lyon", user_id: user6.id)
+teacher5 = Teacher.new(description: "Graduated from a business school, I worked for two years in a startup as business analyst and then as business developper. I then opened a bar with some close friends two years ago. Main interests are skateboarding, music and chess.", specialty: "RAILS", price: "285", city_name: "Lyon", user_id: user6.id)
 teacher5.photo.attach(io: File.open(Rails.root.join('app/assets/images/Hugo_mancini.jpg')), filename: 'Hugo_mancini.jpg')
 teacher5.save
 
