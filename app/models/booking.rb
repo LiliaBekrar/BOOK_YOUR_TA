f56c377a4ba39e343f49c9e2844e9d3f2023e7ca
Lilia Bekrar <liliarossi2504@gmail.com>
2022-03-28 07:17:04 +0200

    test

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :teacher
   belongs_to :user
-
   validates :date, presence: true, uniqueness: { scope: :user_id }
   validates :teacher_id, uniqueness: { scope: :date }
 
